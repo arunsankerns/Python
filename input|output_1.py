104f9d73d21113dd2a97933460ef7c6337f5f2b8
ARUN SANKER N S <88034060+arunsankerns@users.noreply.github.com>
2022-02-16 17:35:11 +0530

    Create input|output_1.py

---
+++ input|output_1.py
@@ -0,0 +1,19 @@
+"""
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+To take an integer value
+
+Output Description:
+Print the integer value
+
+Sample Input :
+2
+Sample Output :
+2
+
+"""
+sample_input=input()
+sample_output=sample_input
+print(sample_output)
+
